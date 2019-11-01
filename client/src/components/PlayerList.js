4a578b64d9e4b820487bfa6f9267293aa27c4ffe
Hugo Oliveira <oliveira.hugo.c@gmail.com>
2019-11-01 11:48:57 -0500

    Added chart

--- client/src/components/PlayerList.js
+++ client/src/components/PlayerList.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import axios from "axios";
 import PlayerCard from "./PlayerCard";
+import PlayerChart from "./PlayerChart";
 
 function PlayerList(props){
 
     return(
         <>PlayerList
+        <PlayerChart players={props.players}/>
         {props.players.map(player => (
+            
             <PlayerCard key={player.id} player={player}/>
         ))}
         </>
